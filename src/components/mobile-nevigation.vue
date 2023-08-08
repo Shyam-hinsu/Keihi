c0e9ae38c368dd46d2c16b19fe4ba6303a25439d
Shyam Hinsu <shyam.hinsu@motadata.com>
2023-08-08 10:09:02 +0530

    make mobile nevigation

--- src/components/mobile-nevigation.vue
+++ src/components/mobile-nevigation.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class='nevigation'>
+  <div class="nevigation">
     <ul>
-
-        <li>
-
-            <a>
-                <span></span>
-            </a>
-        </li>
+      <li class="list">
+        <a href="#">
+          <span class="icon">
+            <font-awesome-icon icon="fa-solid fa-user-secret"
+          /></span>
+          <span class="text">HOME</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="#">
+          <span class="icon">
+            <font-awesome-icon icon="fa-solid fa-user-secret"
+          /></span>
+          <span class="text">DASHBOARD</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="#">
+          <span class="icon">
+            <font-awesome-icon icon="fa-solid fa-user-secret"
+          /></span>
+          <span class="text">REPORT</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="#">
+          <span class="icon">
+            <font-awesome-icon icon="fa-solid fa-user-secret"
+          /></span>
+          <span class="text">MENU</span>
+        </a>
+      </li>
+      <li class="list">
+        <a href="#">
+          <span class="icon">
+            <font-awesome-icon icon="fa-solid fa-user-secret"
+          /></span>
+          <span class="text">OTHER</span>
+        </a>
+      </li>
     </ul>
   </div>
 </template>
