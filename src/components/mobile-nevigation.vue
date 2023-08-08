00b96ec5e9c3f46d854eb674c3fce6217bd7f99b
Shyam Hinsu <shyam.hinsu@motadata.com>
2023-08-08 09:19:49 +0530

    add bottom nev

---
+++ src/components/mobile-nevigation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class='nevigation'>
+    <ul>
+
+        <li>
+
+            <a>
+                <span></span>
+            </a>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
